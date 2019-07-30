9b7a81d30d169525548d9f1046d1ab06aad0453d
Brian Cilenti <bcilenti@administrators-MacBook-Pro-2.local>
2019-07-29 23:36:39 -0400

    bring in useFetch boiler

--- src/comps/Deck/utils.js
+++ src/comps/Deck/utils.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+// import axios from 'axios';
 import mtg from 'mtgsdk';
 
 export const buildUrlArray = async (listString = '') => {
@@ -20,3 +22,23 @@ export async function getCards () {
 
     console.log('Finished!');
 }
+
+export const useFetch = randomTodo => {
+    // console.log('\n', '\n', `useFetch ran = `, randomTodo, '\n', '\n');
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true);
+    useEffect(
+        () => {
+            setLoading(true);
+            // axios
+            //     .get('https://jsonplaceholder.typicode.com/todos/' + randomTodo)
+            //     .then(res => {
+            //         // console.log('\n', '\n', `useFetch, res = `, res, '\n', '\n');
+            //         setData(res.data);
+            //         setLoading(false);
+            //     });
+        },
+        [randomTodo, setData]
+    );
+    return { data, loading };
+};
